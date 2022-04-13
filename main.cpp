bc0b7a3559eb92c157c101fd3e38ba989c17db54
RaikiWatanabe <k021g1324@g.neec.ac.jp>
2022-04-13 11:07:57 +0900

    ウィンドウプロシージャ

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <Windows.h>
 
+//ウィンドウプロージャ
+LRESULT WindowProc(HWND hwnd,UINT msg,WPARAM wparam,LPARAM lparam) {
+	//メッセージに応じてゲーム固有の処理を行う
+	switch (msg) {
+	//ウィンドウが破棄された
+	case WM_DESTROY:
+		//OSも対して、アプリの終了を伝える
+		PostQuitMessage(0);
+		return 0;
+	}
+	//標準のメッセージ処理を行う
+	return DefWindowProc(hwnd,msg,wparam,lparam);
+}
+
 //Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int) {
 	//コンソールへの文字出力
