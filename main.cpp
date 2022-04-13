7a5141ea1969ede8bb9cf2c67d220049d0317eb5
RaikiWatanabe <k021g1324@g.neec.ac.jp>
2022-04-13 13:15:38 +0900

    ウィンドウの生成

--- main.cpp
+++ main.cpp
@@ -16,6 +16,39 @@ LRESULT WindowProc(HWND hwnd,UINT msg,WPARAM wparam,LPARAM lparam) {
 
 //Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int) {
+	const int window_width = 1200; //横幅
+	const int window_height = 720; //縦幅
+
+	//ウィンドウクラスの設定
+	WNDCLASSEX w{};
+	w.cbSize = sizeof(WNDCLASSEX);
+	w.lpfnWndProc = (WNDPROC)WindowProc;	//ウィンドウプロシージャを設定
+	w.lpszClassName = L"DirectXGame";	//ウィンドウクラス名
+	w.hInstance = GetModuleHandle(nullptr);	//ウィンドウハンドル
+	w.hCursor = LoadCursor(NULL, IDC_ARROW);	//カーソル指定
+
+	//ウィンドウクラスを05に登録する
+	RegisterClassEx(&w);
+	//ウィンドウサイズ{ X座標 Y座標 横幅 縦幅 }
+	RECT wrc = { 0,0,window_width,window_height };
+	//自動でサイズを補正する
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+	
+	//ウィンドウオブジェクトの生成
+	HWND hwnd = CreateWindow(w.lpszClassName,	//クラス名
+		L"DirectXGame",							//タイトルバーの文字
+		WS_OVERLAPPEDWINDOW,					//標準的なウィンドウスタイル
+		CW_USEDEFAULT,							//表示X座標(OSに任せる)
+		CW_USEDEFAULT,							//表示Y座標(OSに任せる)
+		wrc.right - wrc.left,					//ウィンドウ横幅
+		wrc.bottom - wrc.top,					//ウィンドウ縦幅
+		nullptr,								//親ウィンドウハンドル
+		nullptr,								//メニューハンドル
+		w.hInstance,							//呼び出しアプリケーションハンドル
+		nullptr);								//オプション
+
+	//ウィンドウを表示状態にする
+	ShowWindow(hwnd,SW_SHOW);
 	//コンソールへの文字出力
 	OutputDebugStringA("Hello,DirectX!!\n");
 
