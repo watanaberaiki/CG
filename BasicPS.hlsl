fa3a702747fd96ff14dfcf9e419aa0a720fa5e99
RaikiWatanabe <k021g1324@g.neec.ac.jp>
2022-05-18 14:19:38 +0900

    ピクセルシェーダ

--- BasicPS.hlsl
+++ BasicPS.hlsl
@@ -1,10 +1,8 @@
-cbuffer ConstBufferDataMaterial : register(b0)
-{
-	float4 color; // 色(RGBA)
-};
+#include "Basic.hlsli"
 
+Texture2D<float4> tex : register(t0);  	// 0番スロットに設定されたテクスチャ
+SamplerState smp : register(s0);      	// 0番スロットに設定されたサンプラー
 
-float4 main() : SV_TARGET
-{
-	return color;
+float4 main(VSOutput input) : SV_TARGET{
+    return float4(tex.Sample(smp, input.uv));
 }
