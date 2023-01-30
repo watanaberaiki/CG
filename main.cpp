33db08d865eb4f96860de438f79e2e12d4321713
watanaberaiki <k021g1324@g.neec.ac.jp>
2023-01-31 02:23:08 +0900

    途中

--- main.cpp
+++ main.cpp
@@ -22,144 +22,6 @@
 
 #pragma comment (lib,"d3dcompiler.lib")
 
-using namespace Microsoft::WRL;
-using namespace DirectX;
-
-//using namespace Microsoft::WRL;
-//using namespace DirectX;
-//#pragma comment (lib,"d3dcompiler.lib")
-//#define DIRECTINPUT_VERSION     0x0800   // DirectInputのバージョン指定
-//#include <dinput.h>
-//
-//#pragma comment(lib, "dinput8.lib")
-//#pragma comment(lib, "dxguid.lib")
-//
-//#pragma comment(lib,"d3d12.lib")
-//#pragma comment(lib,"dxgi.lib")
-//
-//const float PI = 3.141592f;
-//
-////定数バッファ用データ構造体(マテリアル)
-//struct ConstBufferDataMaterial {
-//	XMFLOAT4 color;		//色(RGBA)
-//};
-//
-////定数バッファ用データ構造体(3D変換行列)
-//struct ConstBufferDataTransform {
-//	XMMATRIX mat;		//3D変換行列
-//};
-//
-////3Dオブジェクト型
-//struct Object3d
-//{
-//	//定数バッファ(行列用)
-//	ComPtr<ID3D12Resource> constBuffTransform;
-//	//定数バッファマップ(行列用)
-//	ConstBufferDataTransform* constMapTransform = 0;
-//	//アフィン変換情報
-//	XMFLOAT3 scale = { 1,1,1 };
-//	XMFLOAT3 rotation = { 0,0,0 };
-//	XMFLOAT3 position = { 0,0,0 };
-//	//ワールド変換行列
-//	XMMATRIX matWorld = {};
-//	//親オブジェクトへのポインタ
-//	Object3d* parent = nullptr;
-//};
-//
-//
-////3Dオブジェクトの初期化
-//void InitializeObject3d(Object3d* object, ID3D12Device* device) {
-//	HRESULT result;
-//
-//	//定数バッファのヒープ設定
-//	D3D12_HEAP_PROPERTIES heapProp{};
-//	heapProp.Type = D3D12_HEAP_TYPE_UPLOAD;
-//	// 定数バッファのリソース設定
-//	D3D12_RESOURCE_DESC resdesc{};
-//	resdesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
-//	resdesc.Width = (sizeof(ConstBufferDataTransform) + 0xff) & ~0xff;
-//	resdesc.Height = 1;
-//	resdesc.DepthOrArraySize = 1;
-//	resdesc.MipLevels = 1;
-//	resdesc.SampleDesc.Count = 1;
-//	resdesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
-//
-//	// 定数バッファの生成
-//	result = device->CreateCommittedResource(
-//		&heapProp, // ヒープ設定
-//		D3D12_HEAP_FLAG_NONE,
-//		&resdesc, // リソース設定
-//		D3D12_RESOURCE_STATE_GENERIC_READ,
-//		nullptr,
-//		IID_PPV_ARGS(&object->constBuffTransform));
-//	assert(SUCCEEDED(result));
-//
-//	// 定数バッファのマッピング
-//	result = object->constBuffTransform->Map(0, nullptr, (void**)&object->constMapTransform); // マッピング
-//	assert(SUCCEEDED(result));
-//
-//}
-//
-//void UpdateObject3d(Object3d* object, XMMATRIX& matView, XMMATRIX& matProjection) {
-//
-//	XMMATRIX matScale, matRot, matTrans;
-//
-//	//スケール、回転、平行移動行列の計算
-//	matScale = XMMatrixScaling(object->scale.x, object->scale.y, object->scale.z);
-//
-//	matRot = XMMatrixIdentity();
-//	matRot *= XMMatrixRotationZ(object->rotation.z);
-//	matRot *= XMMatrixRotationX(object->rotation.x);
-//	matRot *= XMMatrixRotationY(object->rotation.y);
-//
-//	matTrans = XMMatrixTranslation(object->position.x, object->position.y, object->position.z);
-//
-//	//ワールド行列の合成
-//	object->matWorld = XMMatrixIdentity();
-//	object->matWorld *= matScale;
-//	object->matWorld *= matRot;
-//	object->matWorld *= matTrans;
-//
-//	//親オブジェクトがあれば
-//	if (object->parent != nullptr)
-//	{
-//		//親オブジェクトのワールド行列をかける
-//		object->matWorld *= object->parent->matWorld;
-//	}
-//
-//	//定数バッファへデータ転送
-//	object->constMapTransform->mat = object->matWorld * matView * matProjection;
-//
-//}
-//
-//void DrawObject3d(Object3d* object, ID3D12GraphicsCommandList* commandList, D3D12_VERTEX_BUFFER_VIEW& vbView,
-//	D3D12_INDEX_BUFFER_VIEW& ibView, UINT numIndices)
-//{
-//	//頂点バッファの設定
-//	commandList->IASetVertexBuffers(0, 1, &vbView);
-//	//インデックスバッファの設定
-//	commandList->IASetIndexBuffer(&ibView);
-//	//定数バッファビュー
-//	commandList->SetGraphicsRootConstantBufferView(2, object->constBuffTransform->GetGPUVirtualAddress());
-//
-//	//描画コマンド
-//	commandList->DrawIndexedInstanced(numIndices, 1, 0, 0, 0);
-//}
-
-////ウィンドウプロージャ
-//LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
-//	//メッセージに応じてゲーム固有の処理を行う
-//	switch (msg) {
-//		//ウィンドウが破棄された
-//	case WM_DESTROY:
-//		//OSも対して、アプリの終了を伝える
-//		PostQuitMessage(0);
-//		return 0;
-//	}
-//	//標準のメッセージ処理を行う
-//	return DefWindowProc(hwnd, msg, wparam, lparam);
-//}
-
 enum Scene {
 	title,
 	game,
@@ -215,18 +77,20 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	input = new Input();
 	input->Initialize(winApp);
 
-	//3Dオブジェクト静的初期化
-	Object3d::StaticInitialize(dxCommon->GetDevice(), winApp->window_width, winApp->window_height);
-
-
 	SpriteCommon* spriteCommon = nullptr;
 	//スプライト共通部分の初期化
 	spriteCommon = new SpriteCommon;
 	spriteCommon->Initialize(dxCommon);
+	
+	//3Dオブジェクト静的初期化
+	Object3d::StaticInitialize(dxCommon->GetDevice(), winApp->window_width, winApp->window_height);
 
-//基盤システムの初期化
 
-//最初のシーンの初期化
+
+
+	//基盤システムの初期化
+
+	//最初のシーンの初期化
 	//テキスト
 	Sprite* sprite = new Sprite();
 	sprite->Initialize(spriteCommon);
@@ -337,7 +201,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		sprite->Draw();
 		sprite1->Draw();
 
-	//最初のシーンの描画
+		//最初のシーンの描画
 
 		dxCommon->PostDraw();
 
